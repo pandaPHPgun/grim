80e42386eb822fdd008bf58524c792fc175e015e
pandaphpgun <panda_gun@i.ua>
2014-05-11 14:27:48 +0300

    first commit

---
+++ header.php
@@ -0,0 +1,199 @@
+
+
+
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible"content="IE=edge">
+  <meta name="viewport"content="width=device-width, initial-scale=1">
+  <title>Grim World</title>
+
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css"rel="stylesheet">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn"t work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+  <!--Top menu-->
+  <nav class="navbar navbar-inverse navbar-fixed-top"role="navigation">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button"class="navbar-toggle"data-toggle="collapse"data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Навигация</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand"href="#">
+          <img src="logo.png"height="" width="" />
+        </a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse"id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active">
+            <a href="#">Главная</a>
+          </li>
+
+          <li class="dropdown">
+            <a  href="#" class="dropdown-toggle"data-toggle="dropdown">
+              Жизнь на сервере <b class="caret"></b>
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <a href="admins.html">Администрация</a>
+              </li>
+              <li class="divider"></li>
+              <li>
+                <a href="#">Общение</a>
+              </li>
+              <li class="divider"></li>
+              <li>
+                <a href="#">Список новых квент</a>
+              </li>
+              <li class="divider"></li>
+              <li>
+                <a href="#">Отправить квенту</a>
+              </li>
+
+            </ul>
+
+          </li>
+          <li class="dropdown">
+            <a  href="#" class="dropdown-toggle"data-toggle="dropdown">
+              Как начать играть <b class="caret"></b>
+            </a>
+            <ul class="dropdown-menu">
+
+              <li>
+
+                <a href="http://www.rutor.org/torrent/314811/gotika-2-zolotoe-izdanie_gothic-2-gold-bonus-2003-pc-repack"> <span class="glyphicon glyphicon-floppy-disk"></span> Торрент с игрой</a>
+              </li>
+              <li class="divider"></li>
+              <li>
+                <a href="https://vk.com/away.php?to=http%3A%2F%2Fdystrybucja.cdp.pl%2Ffiles%2Fget%2F900%2FGothic_Multiplayer"><span class="glyphicon glyphicon-folder-open"></span> Launcher</a>
+              </li> 
+              <li class="divider"></li>
+              <li>
+                <a href="https://disk.yandex.ua/public/?hash=cEXHMk6rpo%2BR4a2Q2VRaG%2B0LkY9n28E7hcCWergL%2B5Q%3D"><span class="glyphicon glyphicon-save"></span> ПАК</a>
+              </li>
+
+            </ul>
+
+          </li>
+          <li>
+            <a href="#">Форум</a>
+          </li>
+          <li>
+            <a href="#">Галарея</a>
+          </li>
+        </ul>
+
+        <ul class="nav navbar-nav navbar-right">
+
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle"data-toggle="dropdown">
+              Моды
+              <b class="caret"></b>
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <a href="#"><span class="glyphicon glyphicon-tree-deciduous"></span> Текстуры</a>
+
+              </li>
+              <li class="divider"></li>
+              <li>
+                <a href="#"><span class="glyphicon glyphicon-font"></span> Шрифты</a>
+              </li>
+
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle"data-toggle="dropdown">
+              Обучение
+              <b class="caret"></b>
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <a href="#"><span class="glyphicon glyphicon-film"></span> Видеогайды</a>
+              </li>
+              <li class="divider"></li>
+              <li>
+                <a href="#"><span class="glyphicon glyphicon-file"></span> Номенклатура</a>
+              </li>
+              <li class="divider"></li>
+              <li>
+                <a href="#"><span class="glyphicon glyphicon-exclamation-sign"></span> Правила</a>
+              </li>
+              <li class="divider"></li>
+              <li>
+                <a href="#"><span class="glyphicon glyphicon-info-sign"></span> Советы</a>
+              </li>
+            </ul>
+          </li>
+
+        </ul>
+
+      </div>
+      <!-- /.navbar-collapse --> </div>
+    <!-- /.container-fluid --> </nav>
+
+
+<div class="container">
+<div class="row"style="margin-top:70px">
+<div class="span12">
+
+
+
+   <div class="container" >
+        <div class="row"style="margin-bottom: 10px;">
+        <div class="span12">
+        <div class="col-md-12">
+          <div id="myCarousel"class="carousel slide"style="#navbar-header"align="center"  >
+
+            <ol class="carousel-indicators">
+              <li class="active"data-target="#myCarousel"data-slide-to="0" ></li>
+              <li data-target="#myCarousel"data-slide-to="1" ></li>
+              <li data-target="#myCarousel"data-slide-to="2" ></li>
+
+            </ol>
+
+            <div class="carousel-inner">
+              <div class="item active">
+                <img alt="" src="img/1.jpg">
+                <div class="carousel-caption"></div>
+              </div>
+              <div class="item">
+                <img alt="" src="img/2.jpg">
+                <div class="carousel-caption"></div>
+              </div>
+              <div class="item">
+                <img alt="" src="img/1.jpg">
+                <div class="carousel-caption"></div>
+              </div>
+
+            </div>
+            <a class="left carousel-control"data-slide="prev"href="#myCarousel">
+              <span class="glyphicon glyphicon-chevron-left"></span>
+
+            </a>
+            <a class="right carousel-control"data-slide="next"href="#myCarousel">
+
+              <span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
